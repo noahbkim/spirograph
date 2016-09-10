4f62b1ee0da791128f65e545fcf994fbb7f1ed86
Noah Kim <noahbkim@gmail.com>
2016-09-09 22:27:57 -0400

    Need a double canvas.

---
+++ spirograph.js
@@ -0,0 +1,33 @@
+var count = 0;
+var lengths = [];
+var thetas = [];
+var velocities = [];
+var last = [];
+var lines = [];
+
+function reset() {
+    lengths = new Array(count);
+    thetas = new Array(count);
+    velocities = new Array(count);
+    last = new Array(count);
+    for (var i = 0; i < count; i++)
+        lengths[i] = thetas[i] = velocities[i] = 0;
+}
+
+function update() {
+    var x, y;
+    for (var i = 0; i < count; i++) {
+        
+        
+    }
+}
+
+function draw() {
+    
+}
+
+window.onload = function() {
+
+    
+    
+}

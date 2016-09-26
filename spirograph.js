b8e46471d2251e157e9da4cd817909ab5f0a296a
Noah Kim <noahbkim@gmail.com>
2016-09-25 22:20:56 -0400

    Thanks for the math help.

--- spirograph.js
+++ spirograph.js
@@ -1,37 +1,64 @@
 function gcd(a, b) { return !b ? a : gcd(b, a % b); }
 function lcm(a, b) { return (a * b) / gcd(a, b); }
+function resize(array, length, fill) {
+    var resized = new Array(length);
+    for (var i = 0; i < length; i++) {
+        if (i < array.length) resized[i] = array[i];
+        else resized[i] = fill || 0;
+    }
+    return resized;
+}
 
 function Engine(canvas) {
     
     var context = canvas.getContext("2d");
-    this.count = 3;
-    this.velocities = [0.5, 1.0, 2.0];
-    this.angles = [0, 0, 0];
-    this.lengths = [30, 30, 30];
     this.points = [];
     
-    
-    var total = ;
-    var z = 0;
+    this.count = 1;
+    this.velocities = [0];
+    this.angles = [0];
+    this.lengths = [20]
+        
+    this.completion = 0;
+    this.current = 0;
     
     var x, y;
     
     // LCM {LCM a 360} {LCM b 360} {LCM c 360}  
     
-    this.draw = function() {
-        
-        requestAnimationFrame(this.draw.bind(this));
-        
-        console.log(z, total);
-        if (++z >= total) return
-        
-        for (var i = 0; i < this.count; i++)
-            //for (var j = i; j < this.count; j++)
-                this.angles[i] += this.velocities[i];
-        
-        context.clearRect(0, 0, canvas.width, canvas.height);
+    this.setCount = function(n) {
+        this.count = n;
+        this.velocities = resize(this.velocities, n);
+        this.angles = resize(this.angles, n);
+        this.lengths = resize(this.lengths, n, 20);
+    }
+    
+    this.setVelocities = function(v) {
+        this.reset();
+        var gcds = [];
+        for (var i = 0; i < this.count; i++) {
+            this.velocities[i] = v[i];
+            gcds[i] = 360 / gcd(v[i] || 0, 360);
+        }
+        this.completion = gcds.reduce(lcm) + 1;
+    }
+    
+    this.reset = function() {
+        this.current = 0;
+        this.clear();
+        for (var i = 0; i < this.count; i++) {
+            this.velocities[i] = 1;
+            this.angles[i] = 0;
+            this.lengths[i] = 20;
+        }
+    }
+    
+    this.clear = function() {
+        this.points = [];
+    }
+    
+    this.drawArms = function() {
         context.beginPath();
-        
         x = canvas.width / 2;
         y = canvas.height / 2;
         context.moveTo(x, y);
@@ -42,9 +69,12 @@ function Engine(canvas) {
             y += length * Math.sin(angle * Math.PI / 180);
             context.lineTo(x, y);
         }
-        
-        this.points.push([x, y]);
-        
+        this.points.push([x, y]);  // Also writes the new point
+        context.stroke();
+    }
+    
+    this.drawPoints = function() {
+        context.beginPath();
         if (this.points.length <= 1) return;
         var first = this.points[0];
         context.moveTo(first[0], first[1]);
@@ -52,8 +82,22 @@ function Engine(canvas) {
             var next = this.points[i];
             context.lineTo(next[0], next[1]);
         }
-        
         context.stroke();
+    }
+    
+    this.draw = function() {
+        
+        requestAnimationFrame(this.draw.bind(this));
+        
+        for (var i = 0; i < this.count; i++)
+            this.angles[i] += this.velocities[i];
+        
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        
+        if (this.current++ < this.completion) 
+            this.drawArms();
+        
+        this.drawPoints();
         
     }
     
@@ -63,5 +107,7 @@ var engine;
 
 window.onload = function() {
     engine = new Engine(document.getElementById("canvas"));
+    engine.setCount(3);
+    engine.setVelocities([1.5, 2.0, 2.5])
     engine.draw();
 }

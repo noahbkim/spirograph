48c459e2aaced3a5a831facef38d0b590312b8c6
Noah Kim <noahbkim@gmail.com>
2017-03-24 18:59:04 -0400

    Should work...

--- controls.js
+++ controls.js
@@ -4,6 +4,10 @@ const e = {
     wrapper: document.getElementById("wrapper"),
     controls: document.getElementById("controls"),
     menu: document.getElementById("menu"),
+    arms: document.getElementById("arms"),
+    independent: document.getElementById("independent"),
+    infinite: document.getElementById("infinite"),
+    add: document.getElementById("add"),
 };
 
 /* Bind to menu click. */
@@ -13,8 +17,119 @@ e.menu.addEventListener("click", function() {
     e.menu.classList.toggle("centered");
 });
 
-
 const engine = new Engine(e.canvas);
+
+function addArm(arm) {
+    engine.add(arm);
+    e.arms.insertBefore(armToHTML(arm, engine.count), e.arms.lastElementChild);
+    engine.play();
+}
+
+function removeArm(arm) {
+    let index = engine.remove(arm);
+    e.arms.removeChild(e.arms.children[index]);
+    engine.play();
+}
+
+/* Create HTML controls for single arm. */
+function armToHTML(arm, number) {
+
+    /* This is why Angular was created... */
+    let div = document.createElement("div");
+    div.classList.add("arm");
+    let header = document.createElement("span");
+    header.innerHTML += "Arm " + number;
+    div.appendChild(header);
+
+    let menubar = document.createElement("span");
+    menubar.classList.add("menubar", "right");
+    header.appendChild(menubar);
+    let dropdown = document.createElement("span");
+    dropdown.classList.add("dropdown");
+    dropdown.innerHTML = "&#x276C;";
+    menubar.appendChild(dropdown);
+
+    if (number > 1) {
+        let remove = document.createElement("span");
+        remove.classList.add("remove");
+        remove.innerHTML = "&#x2a09;";
+        remove.arm = arm;
+        menubar.appendChild(remove);
+        remove.addEventListener("click", function() {
+            removeArm(this.arm);
+        });
+    }
+
+    let controls = document.createElement("div");
+    controls.classList.add("controls");
+    div.appendChild(controls);
+
+    let lengthLabel = document.createElement("label");
+    lengthLabel.innerHTML = "Length: ";
+    controls.appendChild(lengthLabel);
+    let lengthValue = document.createElement("span");
+    lengthValue.innerHTML = arm.length;
+    lengthLabel.appendChild(lengthValue);
+    let lengthInput = document.createElement("input");
+    lengthInput.type = "range";
+    lengthInput.min = "5";
+    lengthInput.max = "50";
+    lengthInput.step = "1";
+    lengthInput.value = arm.length;
+    lengthInput.arm = arm;
+    controls.appendChild(lengthInput);
+    lengthInput.addEventListener("input", function() {
+        lengthValue.innerHTML = this.value;
+    });
+    lengthInput.addEventListener("change", function() {
+        this.arm.length = parseInt(this.value);
+        engine.reset();
+        engine.play();
+    });
+
+    let velocityLabel = document.createElement("label");
+    velocityLabel.innerHTML = "Velocity: ";
+    controls.appendChild(velocityLabel);
+    let velocityValue = document.createElement("span");
+    velocityValue.innerHTML = arm.velocity;
+    velocityLabel.appendChild(velocityValue);
+    let velocityInput = document.createElement("input");
+    velocityInput.type = "range";
+    velocityInput.min = "-3";
+    velocityInput.max = "3";
+    velocityInput.step = "0.1";
+    velocityInput.value = arm.velocity;
+    velocityInput.arm = arm;
+    velocityInput.addEventListener("input", function() {
+        velocityValue.innerHTML = this.value;
+    });
+    velocityInput.addEventListener("change", function() {
+        this.arm.velocity = parseFloat(this.value);
+        engine.reset();
+        engine.play();
+    });
+    controls.appendChild(velocityInput);
+
+    header.classList.add("header");
+    return div;
+}
+
+e.independent.addEventListener("change", function() {
+    engine.indpendent = this.checked;
+    engine.reset();
+    engine.play();
+});
+e.independent.checked = engine.independent;
+
+e.infinite.addEventListener("change", function() {
+    engine.infinite = this.checked;
+});
+e.infinite.checked = engine.infinite;
+
+e.add.addEventListener("click", function() {
+    addArm(new Arm(20, 0));
+});
+
 
 e.canvas.addEventListener("click", function() {
     engine.reset();
@@ -23,6 +138,7 @@ e.canvas.addEventListener("click", function() {
 
 window.onload = function() {
     engine.start();
-    engine.add(20, 2.0);
+    let arm = new Arm(20, 2.0);
+    addArm(arm);
     engine.play();
 };
--- spirograph.js
+++ spirograph.js
@@ -13,13 +13,6 @@ class Arm {
         this.angle = 0;
     }
 
-    toHTML() {
-        let div = document.createElement("div");
-        div.classList.add("arm");
-
-        return div;
-    }
-
 }
 
 class Engine {
@@ -30,37 +23,35 @@ class Engine {
         this.points = [];
         this.arms = [];
         this.precision = 2;
-        this.independent = false;
+        this.independent = true;
+        this.infinite = false;
+        this.paused = false;
+
         this.completion = 0;
         this.current = 0;
-        this.paused = false;
+        this.finished = false;
+    }
+
+    get count() {
+        return this.arms.length;
     }
 
     /* Add an arm to the spirograph. */
-    add(l, v) {
+    add(arm) {
         this.pause();
-        this.count += 1;
-        let arm = new Arm(l, v);
         this.arms.push(arm);
         this.reset();
         return arm;
     }
 
-    /* Modify an arm of the spirograph. */
-    modify(i, l, v) {
-        this.pause();
-        this.arms[i].length = l || this.arms[i].length;
-        this.arms[i].velocity = v || this.arms[i].velocity;
-        this.reset();
-    }
-
     /* Remove an arm of the spirograph. */
-    remove() {
+    remove(arm) {
         this.pause();
         if (this.count <= 1) return;
-        this.count -= 1;
-        this.arms.pop();
+        let index = this.arms.indexOf(arm);
+        this.arms.splice(index, 1);
         this.reset();
+        return index;
     }
 
     pause() { this.paused = true; }
@@ -76,7 +67,6 @@ class Engine {
         let gcds = [];
         for (let i = 0; i < this.arms.length; i++)
             gcds[i] = 360 / gcd(Math.abs(this.arms[i].velocity) || 0, 360);
-        console.log(gcds);
         this.completion = gcds.reduce(lcm) * this.precision + 1;
     }
 
@@ -85,14 +75,10 @@ class Engine {
         this.pause();
         this.current = 0;
         this.clear();
+        this.context.clearRect(0, 0, canvas.width, canvas.height);
         for (let arm of this.arms)
             arm.reset();
         this.recalculate();
-    }
-
-    update() {
-        for (let arm of this.arms)
-            arm.angle += arm.velocity;
     }
 
     drawArms(context, canvas) {
@@ -100,14 +86,18 @@ class Engine {
         let x = canvas.width / 2;
         let y = canvas.height / 2;
         context.moveTo(x, y);
+        let last = 0;
         for (let arm of this.arms) {
             let angle = arm.angle / this.precision;
+            if (!this.independent) angle += last;
+            last = angle;
             let length = arm.length;
             x += length * Math.cos(angle * Math.PI / 180);
             y += length * Math.sin(angle * Math.PI / 180);
             context.lineTo(x, y);
         }
-        this.points.push([x, y]);
+        if (!this.finished)
+            this.points.push([x, y]);
         context.stroke();
     }
     
@@ -123,19 +113,26 @@ class Engine {
     
     draw() {
         requestAnimationFrame(this.draw.bind(this));
+        let canvas = this.canvas;
+        let context = this.context;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        if (!this.finished || this.infinite)
+            this.drawArms(context, canvas);
+        this.drawPoints(context, canvas);
+    }
+
+    update() {
         if (!this.paused) {
-            this.update();  // Probably fine in draw loop...
-            let canvas = this.canvas;
-            let context = this.context;
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            if (this.current++ < this.completion)
-                this.drawArms(context, canvas);
-            this.drawPoints(context, canvas);
+            for (let arm of this.arms)
+                arm.angle += arm.velocity;
         }
+        if (this.current++ >= this.completion)
+            this.finished = true;
     }
 
     start() {
         this.draw();
+        setInterval(this.update.bind(this), 1000 / 60);
     }
     
 }
